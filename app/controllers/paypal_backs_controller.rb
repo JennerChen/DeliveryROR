65328e6e3887f3a19fdb4c5e41a615c36bcdc0c1
ZHANG QING 20060883 <zhangqing332@live.com>
2014-04-29 00:58:32 +0100

    fix paypalnotification nameerror

--- app/controllers/paypal_backs_controller.rb
+++ app/controllers/paypal_backs_controller.rb
@@ -2,6 +2,7 @@ class PaypalBacksController < ApplicationController
 	def create
 		@order = Order.find_by_id(params[:invoice])
 		if @order
+			# PaymentNotification.create!(:params => params, :order_id => params[:invoice], :status => params[:payment_status], :transcation_id => params[:txn_id])
 			flash[:success] = "order purchased"
 			redirect_to "/orders/#{@order.id}"
 		else
